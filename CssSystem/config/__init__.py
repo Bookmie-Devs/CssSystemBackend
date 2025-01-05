6421b6b1ea49226d6d80099eaadfc83d09bdf416
ethenkem <ethenatx@gmail.com>
2025-01-05 18:28:25 +0000

    sms templates

--- CssSystem/config/__init__.py
+++ CssSystem/config/__init__.py
@@ -1,3 +1,4 @@
 from CssSystem.config.jazzmin import *
 from CssSystem.config.maps import *
 from CssSystem.config.simple_jwt import *
+from CssSystem.config.sms import *
---
+++ CssSystem/config/sms.py
@@ -0,0 +1,6 @@
+from decouple import config
+
+# SMS API KEYS
+SMS_API_KEY = config("SMS_API_KEY")
+
+SENDER_ID = config("SENDER_ID")
